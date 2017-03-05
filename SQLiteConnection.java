9949997ae3935765857f65093af77cb11767c83f
Nolan Dickson <nolandickson@cmail.carleton.ca>
2017-03-05 04:44:52 -0500

    Ready to use.

--- SQLiteConnection.java
+++ SQLiteConnection.java
@@ -1,20 +1,43 @@
 package hera;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class SQLiteConnection {
 
-    public static void connect() throws Exception {
+	
+	public SQLiteConnection() throws Exception{
+	}
+	public void userTable(String name, int amount, int paid) throws Exception{
+		String s = "insert into Person(Name, AmountOwed, Paid) values('" + name +"', '"+Integer.toString(amount)+"', '"+Integer.toString(paid)+"')";
+		connect(s);
+	}
+	public void houseTable(int people, int amount, int bills) throws Exception{
+		String s = "insert into House(People, BillAmount, Bills) values('" + Integer.toString(people) +"', '"+Integer.toString(amount)+"', '"+Integer.toString(bills)+"')";
+		System.out.println(s);
+		connect(s);
+	}
+	public void billTable(String name, int amount, Date date, int paid) throws Exception{
+		String s = "insert into Person(Name, AmountOwed, Paid) values('" + name +"', '"+Integer.toString(amount)+"', '"+date.toString()+"', '"+Integer.toString(paid)+"')";
+		connect(s);
+	}
+    public void connect(String s) throws Exception {
         Connection conn = null;
         try {
             // db parameters
             String url = "jdbc:sqlite:C:/Users/Nolan/workspace/hera/hera.db";
             // create a connection to the database
             conn = DriverManager.getConnection(url);
+            Statement statement = conn.createStatement();
+            statement.setQueryTimeout(30);  // set timeout to 30 sec.
             
             System.out.println("Connection to SQLite has been established.");
             
+            statement.executeUpdate(s);
+            ResultSet rs = statement.executeQuery("select * from person");
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         } finally {
@@ -27,11 +50,8 @@ public class SQLiteConnection {
             }
         }
     }
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) throws Exception{
-        connect();
-    }
+
+	public static void main(String[] args){}
+
 }
 
