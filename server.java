810f635792e5b621d0b6d36be5ce3fa537757d40
Nolan Dickson <nolandickson@cmail.carleton.ca>
2017-03-04 20:28:43 -0500

    Functioning Server - Enterprise Style

--- server.java
+++ server.java
@@ -1,29 +1,45 @@
 package hera;
-/* Project Hera - 0.1
+/* Project Hera - 0.6
  * Nolan Dickson - 100956964
  * 
- * March 4, 2017 - This is a very simple test of the capabilities of dealing with
- * Http; sending and receiving to a ServerSocket.
+ * March 4, 2017 - This is a very simple test of the capabilities of 
+ * Http; sending and receiving with a client.
  * 
 */
 import java.io.*;
 import java.net.*;
+import java.util.concurrent.TimeUnit;
 import java.net.ServerSocket;
 
 public class server{
-	public static void main(String[] args) throws Exception {
-		
-		final ServerSocket server  = new ServerSocket(8080);
+	
+	// Local variables
+	private int localPort;
+	private int bufferSize;
+	private int socketTimeout;
+	
+	// Server 
+	public server(int port) throws Exception{
+		final ServerSocket server  = new ServerSocket(port);
 		System.out.println("Listening for connection on ServerSocket Port...");
 		
+		// Set info
+		localPort = port;
+		setBufferSize(server.getReceiveBufferSize());
+		setSocketTimeout(server.getSoTimeout());
+		
+		// Start client contact
+		clientInteract(server);
+	}// End of Server
+	
+	private void clientInteract(ServerSocket server) throws Exception{
 		while (true){
 			try(final Socket clientSocket = server.accept()){
+				// We're in!
+				System.out.println("Connection Established.");
+				TimeUnit.SECONDS.sleep(1);
+				System.out.println();
 				
-				// Send a string to the accepted client Socket
-				String response = "HTTP/1.1 200 OK\r\n\r\n" + "Hello World!";
-				clientSocket.getOutputStream().write(response.getBytes("UTF-8"));
-			
-			
 				// Setup to receive info from client
 				InputStreamReader isr =  new InputStreamReader(clientSocket.getInputStream());
 				BufferedReader reader = new BufferedReader(isr);
@@ -34,7 +50,36 @@ public class server{
 					System.out.println(line);
 					line = reader.readLine();
 				}
+				
+				// Send a string to the accepted client Socket
+				String response = "HTTP/1.1 200 OK\r\n\r\n" + server.toString();
+				clientSocket.getOutputStream().write(response.getBytes("UTF-8"));
+				
 			}
-		}
+		}	
+	}
+
+	public int getPort(){
+		return localPort;
+	}
+	public int getBufferSize() {
+		return bufferSize;
+	}
+
+	public void setBufferSize(int bufferSize) {
+		this.bufferSize = bufferSize;
+	}
+
+	public int getSocketTimeout() {
+		return socketTimeout;
+	}
+
+	public void setSocketTimeout(int socketTimeout) {
+		this.socketTimeout = socketTimeout;
+	}
+	
+	// Main
+	public static void main(String[] args) throws Exception {
+		server s = new server(8080);
 	}
 }
