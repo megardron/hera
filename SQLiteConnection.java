6bed67b226cf56dad178fa7b8a907f15b44d3a3a
Nolan Dickson <nolandickson@cmail.carleton.ca>
2017-03-05 00:20:28 -0500

    Add files via upload

---
+++ SQLiteConnection.java
@@ -0,0 +1,37 @@
+package hera;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class SQLiteConnection {
+
+    public static void connect() throws Exception {
+        Connection conn = null;
+        try {
+            // db parameters
+            String url = "jdbc:sqlite:C:/Users/Nolan/workspace/hera/hera.db";
+            // create a connection to the database
+            conn = DriverManager.getConnection(url);
+            
+            System.out.println("Connection to SQLite has been established.");
+            
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception{
+        connect();
+    }
+}
+
